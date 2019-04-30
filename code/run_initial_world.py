0d14cc6857b83885d60085f56aa3be83abd4fc21
John Choi <jchoi2@coxautoinc.com>
2019-04-29 18:44:33 -0700

    Initial commit of world

---
+++ code/run_initial_world.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import time
+from malmo import MalmoPython
+
+
+# Create default Malmo objects:
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse(sys.argv)
+except RuntimeError as e:
+    print('ERROR:', e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+missionXML = open('world.xml', 'r')
+my_mission = MalmoPython.MissionSpec(missionXML.read(), True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission(my_mission, my_mission_record)
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:", e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+
+# Loop until mission starts:
+print("Waiting for the mission to start ", end=' ')
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission running ", end=' ')
+
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission ended")
+# Mission has ended.
