b5b4f0373f70e811dbc7662f2211f130df0bf10a
Neil Schultz-Cox <neiltcox@icloud.com>
2019-05-19 09:37:22 -0700

    added ddqn

--- code/run_initial_world.py
+++ code/run_initial_world.py
@@ -1,13 +1,12 @@
-import os
 import sys
 import time
+
 try:
     from malmo import MalmoPython
 except:
     import MalmoPython
 import steve_agent
 import json
-
 
 # Create default Malmo objects:
 
@@ -42,7 +41,6 @@ for retry in range(max_retries):
         else:
             time.sleep(2)
 
-
 # Loop until mission starts:
 print("Waiting for the mission to start ", end=' ')
 world_state = agent_host.getWorldState()
@@ -51,11 +49,10 @@ while not world_state.has_mission_begun:
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
-        print("Error:",error.text)
+        print("Error:", error.text)
 
 print()
 print("Mission running ", end=' ')
-
 
 # Loop until mission ends:
 while world_state.is_mission_running:
@@ -63,18 +60,18 @@ while world_state.is_mission_running:
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
-        print("Error:",error.text)
+        print("Error:", error.text)
 
     if world_state.number_of_observations_since_last_state > 0:
         msg = world_state.observations[-1].text
         ob = json.loads(msg)
         steve = steve_agent.Steve()
-        #all entity_info in a tuple (x, y, z)
-        agent_info = (ob.get(u'XPos', 0), ob.get(u'YPos', 0), ob.get(u'ZPos', 0)) 
-        entities = steve.get_mob_loc(ob) 
+        # all entity_info in a tuple (x, y, z)
+        agent_info = (ob.get(u'XPos', 0), ob.get(u'YPos', 0), ob.get(u'ZPos', 0))
+        entities = steve.get_mob_loc(ob)
         target = steve.closest_enemy(agent_info, entities)
-        #zombie mob height is 1.9 LMAO 
-        target_yaw, target_pitch = steve.calcYawAndPitchToMob(target, agent_info[0], agent_info[1], agent_info[2], 1.9) 
+        # zombie mob height is 1.9 LMAO
+        target_yaw, target_pitch = steve.calcYawAndPitchToMob(target, agent_info[0], agent_info[1], agent_info[2], 1.9)
         pointing = steve.lock_on(agent_host, ob, target_pitch, target_yaw, 5)
 
 print()
--- code/steve_agent.py
+++ code/steve_agent.py
@@ -6,67 +6,67 @@ try:
 except:
     import MalmoPython
 
+
 class Steve(object):
-	def __init__(self, alpha = 0.3, gamma = 1, n = 1):
-		self.eps = 0.2
-		self.q_table = {}
-		self.alpha = alpha
-		self.gamam = gamma 
-		self.n = n
+    def __init__(self, alpha=0.3, gamma=1, n=1):
+        self.eps = 0.2
+        self.q_table = {}
+        self.alpha = alpha
+        self.gamam = gamma
+        self.n = n
 
-	def lock_on(self, agent_host, ob, target_pitch, target_yaw, threshhold):
-		pitch = ob.get(u'Pitch', 0)
-		yaw = ob.get(u'Yaw', 0)
-		delta_yaw = self.angvel(target_yaw, yaw, 50.0)
-		delta_pitch = self.angvel(target_pitch, pitch, 50.0)
-		agent_host.sendCommand("turn " + str(delta_yaw))
-		agent_host.sendCommand("pitch " + str(delta_pitch))
-		"""
-		if abs(pitch-target_pitch) + abs(yaw-target_yaw) < threshhold:
-			print("TRIGGERED")
-			agent_host.sendCommand("turn 0")
-			agent_host.sendCommand("pitch 0")
-			return True
-		"""
-		return False
+    def lock_on(self, agent_host, ob, target_pitch, target_yaw, threshhold):
+        pitch = ob.get(u'Pitch', 0)
+        yaw = ob.get(u'Yaw', 0)
+        delta_yaw = self.angvel(target_yaw, yaw, 50.0)
+        delta_pitch = self.angvel(target_pitch, pitch, 50.0)
+        agent_host.sendCommand("turn " + str(delta_yaw))
+        agent_host.sendCommand("pitch " + str(delta_pitch))
+        """
+        if abs(pitch-target_pitch) + abs(yaw-target_yaw) < threshhold:
+            print("TRIGGERED")
+            agent_host.sendCommand("turn 0")
+            agent_host.sendCommand("pitch 0")
+            return True
+        """
+        return False
 
-	def angvel(self, target, current, scale):
-		delta = target - current
-		while delta < -180:
-			delta += 360
-		while delta > 180:
-			delta -= 360
-		return (old_div(2.0, (1.0 + math.exp(old_div(-delta,scale))))) - 1.0
+    def angvel(self, target, current, scale):
+        delta = target - current
+        while delta < -180:
+            delta += 360
+        while delta > 180:
+            delta -= 360
+        return (old_div(2.0, (1.0 + math.exp(old_div(-delta, scale))))) - 1.0
 
-	def calcYawAndPitchToMob(self, target, x, y, z, target_height): 
-		dx = target[0] - x
-		dz = target[2] - z
-		yaw = -180 * math.atan2(dx, dz) / math.pi
-		distance = math.sqrt(dx * dx + dz * dz)
-		pitch = math.atan2(((y + 1.625) - (target[1] + target_height * 0.9)), distance) * 180.0 / math.pi
-		return yaw, pitch
+    def calcYawAndPitchToMob(self, target, x, y, z, target_height):
+        dx = target[0] - x
+        dz = target[2] - z
+        yaw = -180 * math.atan2(dx, dz) / math.pi
+        distance = math.sqrt(dx * dx + dz * dz)
+        pitch = math.atan2(((y + 1.625) - (target[1] + target_height * 0.9)), distance) * 180.0 / math.pi
+        return yaw, pitch
 
-	def get_mob_loc(self, ob):
-		"""gets the locations of all the entities in world state"""
-		entities = {}
-		for ent in ob["entities"]:
-			if (ent["name"] == "175Project"):
-				continue
-			mob_id = ent['id']
-			entities[mob_id] = (ent['x'], ent['y'], ent['z'])
-		return entities
+    def get_mob_loc(self, ob):
+        """gets the locations of all the entities in world state"""
+        entities = {}
+        for ent in ob["entities"]:
+            if (ent["name"] == "175Project"):
+                continue
+            mob_id = ent['id']
+            entities[mob_id] = (ent['x'], ent['y'], ent['z'])
+        return entities
 
-	def closest_enemy(self, agent, entities):
-		mob_name = ""
-		dist = 10000
-		for mobs in entities.keys():
-			new_dist = self.calculate_distance(agent, entities[mobs])
-			if (dist > new_dist):
-				mob_name = mobs
-				dist = new_dist
-		return entities[mob_name]
+    def closest_enemy(self, agent, entities):
+        mob_name = ""
+        dist = 10000
+        for mobs in entities.keys():
+            new_dist = self.calculate_distance(agent, entities[mobs])
+            if (dist > new_dist):
+                mob_name = mobs
+                dist = new_dist
+        return entities[mob_name]
 
-	def calculate_distance(self, agent, mob):
-		"""Takes the agent and mob's location and calculates distance"""
-		return math.sqrt((agent[0] - mob[0])**2 + (agent[2] - mob[2])**2)
-
+    def calculate_distance(self, agent, mob):
+        """Takes the agent and mob's location and calculates distance"""
+        return math.sqrt((agent[0] - mob[0]) ** 2 + (agent[2] - mob[2]) ** 2)
