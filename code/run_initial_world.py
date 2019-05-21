af636371fc922d734096e0a77f35b103366c3bba
Neil Schultz-Cox <neiltcox@icloud.com>
2019-05-20 19:11:57 -0700

    integrate NN code and make more verbose

--- code/run_initial_world.py
+++ code/run_initial_world.py
@@ -7,6 +7,10 @@ except:
     import MalmoPython
 import steve_agent
 import json
+import configparser
+from ddqn import DQNAgent
+
+config = configparser.ConfigParser()
 
 # Create default Malmo objects:
 
@@ -28,6 +32,12 @@ my_client_pool = MalmoPython.ClientPool()
 my_client_pool.add(MalmoPython.ClientInfo('127.0.0.1', 10000))
 
 EPISODES = 5000
+
+state_size =  config.get('DEFAULT', 'STATE_SIZE')
+action_size =  config.get('DEFAULT', 'ACTION_SIZE')
+agent = DQNAgent(state_size, action_size)
+done = False
+batch_size = config.get('DEFAULT', 'BATCH_SIZE')
 
 for repeat in range(EPISODES):
     my_mission = MalmoPython.MissionSpec(missionXML, True)
@@ -66,6 +76,10 @@ for repeat in range(EPISODES):
 
     steve = steve_agent.Steve()
     # Loop until mission ends:
+
+    # keep track if we've seeded the initial state
+    have_initial_state = 0
+
     while world_state.is_mission_running:
         print(".", end="")
         time.sleep(0.1)
@@ -76,7 +90,6 @@ for repeat in range(EPISODES):
         if world_state.number_of_observations_since_last_state > 0:
             msg = world_state.observations[-1].text
             ob = json.loads(msg)
-            state = steve.get_state(ob)
             # all entity_info in a tuple (x, y, z)
             agent_info = (ob.get(u'XPos', 0), ob.get(u'YPos', 0), ob.get(u'ZPos', 0))
             entities = steve.get_mob_loc(ob)
@@ -84,6 +97,29 @@ for repeat in range(EPISODES):
             # zombie mob height is 1.9 LMAO
             target_yaw, target_pitch = steve.calcYawAndPitchToMob(target, agent_info[0], agent_info[1], agent_info[2], 1.9)
             pointing = steve.lock_on(agent_host, ob, target_pitch, target_yaw, 5)
+
+            # MAIN NN LOGIC
+            # check if we've seeded initial state just for the first time
+            if have_initial_state == 0:
+                state = agent.get_state(ob)
+                have_initial_state = 1
+
+            action = agent.act(state)
+            steve.perform_action(action) # send action to malmo
+            reward = steve.get_reward(); # get reward
+            next_state = steve.get_state(ob) # get next state
+            done = False # mission is never one, keep done False
+            reward = reward if not done else -10
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                agent.update_target_model()
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+            # MAIN NN LOGIC
 
     print()
     print("Mission ended")
